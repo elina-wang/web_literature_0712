d440692b48d135eae37323be6fd33ff24cf4f444
elina-wang <siaosyuan0611@gmail.com>
2020-07-19 16:53:16 +0800

    關於完成

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
 
 
-$("#link2").fadeOut(0);
-$("#link").hover(() => {
-    // $("#mes1").slideDown(2000);
-    $("#link2").delay(500).fadeIn(1000);
+// 最先 link2 摺起來, .動畫 設定 字體透明 0 
 
-
+$("#link2").slideUp(0).animate({
+    "opacity": 0
 });
 
-$("#link2").mouseleave(
-    () => {
-        $("#link2").fadeOut(800);
-
+// 滑至 link , link2摺下來, .動畫 設定 字體透明 1 (顯示)
+$("#link").hover(() => {
+    
+    $("#link2").slideDown(1000).animate({
+        "opacity": 1
     });
+
+    // $("#link2").delay(500).fadeIn(1000);
+    
+});
+
+$("#link2").mouseleave(() => {
+    $("#link2").fadeOut(800);
+});
